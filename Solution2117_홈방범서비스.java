361e457292e1cb086e7a7e800238595f453d45e7
Seungah Yoo <41937599+SeungahYoo@users.noreply.github.com>
2019-08-07 23:54:46 +0900

    swea 홈방범서비스

--- Solution2117_홈방범서비스.java
+++ Solution2117_홈방범서비스.java
@@ -1,55 +1,86 @@
+//2019-08-07
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//SWEA :: 2117 È¨¹æ¹ü¼­ºñ½º
-public class Solution2117_È¨¹æ¹ü¼­ºñ½º {
-	private static int M;
+public class Solution2117_í™ˆë°©ë²”ì„œë¹„ìŠ¤ {
 	private static int N;
+	private static int M, total;
 	private static int[][] map;
-	private static int budget;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine().trim());
+
+		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			N = Integer.parseInt(st.nextToken()); // µµ½ÃÀÇ Å©±â
-			M = Integer.parseInt(st.nextToken()); // ÇÏ³ªÀÇ ÁýÀÌ ÁöºÒÇÒ ¼ö ÀÖ´Â ºñ¿ë
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // ë„ì‹œ í¬ê¸°
+			M = Integer.parseInt(st.nextToken()); // ì§€ë¶ˆ ë¹„ìš©
 			map = new int[N][N];
-			
-			int home = 0;
-			for(int i=0;i<N;i++) {
-				st  = new StringTokenizer(br.readLine(), " ");
-				for(int j=0;j<N;j++) {
+			total = 0;
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					if(map[i][j]==1) home++;
-				} //////////// input
+					if(map[i][j] == 1) {
+						total++;
+					}
+				}
+			} /// input
+			ans = -1;
+
+			for (int i = 1; i < 2*N; i++) { //ë§ˆë¦„ëª¨ í¬ê¸° ê²°ì • 
+				if(solve(i)) break;
 			}
-			
-			//ÃÖ´ë µ· 
-			budget = home*M;
-			
-			//1. ÃÖ´ë K(¼­ºñ½º¿µ¿ª Å©±â) °áÁ¤ÇÏ±â
-			
-			
-			System.out.println("#"+tc+" ");
+			System.out.println("#" + tc + " " + ans);
 		} // end of tc
-	} // end of main
-	
-	public static void solve() {
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
+	}// end of main
+
+	static int ans;
+
+	public static boolean solve(int k) {
+		for (int i = -k; i < N; i++) {
+			for (int j = -N+1; j < N; j++) {
+				int home = safeZone(k, i, j);
 				
+				if (home > 0) {
+					int profit = home * M - (k * k + (k - 1) * (k - 1));
+					if (profit >= 0) {
+						ans = ans < home ? home : ans;
+						if(home == total) return true;
+					}
+				}
 			}
 		}
+		return false;
+	}
+
+	public static int safeZone(int k, int y, int x) {
+		int s = k * 2 - 1;
+		int home = 0;
+		int start = s / 2;
+		int end = s / 2;
+		for (int i = 0; i < s; i++) {
+			for (int j = 0; j < s; j++) {
+				if (j < start || j > end || (y + i) < 0 || (x + j) < 0 || (y + i) > N - 1 || (x + j) > N - 1) {
+					continue;
+				}
+
+				if (map[y + i][x + j] == 1) {
+					home++;
+				}
+			}
+			if (i < s / 2) {
+				start--;
+				end++;
+			} else {
+				start++;
+				end--;
+			}
+		}
+		return home;
 	}
 	
-	public static void makeSafeZone(int y, int x, int k) {
-		for(int i=0;i<N;i++) {
-		}
-	}
-}
+}// end of class
